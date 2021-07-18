45dd5cf872f1b646ebf0fde331d1cfbb5a76dd78
Yaniv Nizry <yanivnizry@gmail.com>
2021-07-18 10:05:22 +0000

    fix withStyles

--- examples/customize-styling/index.js
+++ examples/customize-styling/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MUIDataTable from '../../src/';
-import { createMuiTheme, MuiThemeProvider, withStyles } from '@material-ui/core/styles';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/styles';
 import Switch from '@material-ui/core/Switch';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
